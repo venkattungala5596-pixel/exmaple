e1b999752c0507a2601302ee8c9185b046e98610
Halesh Tungala <haleshtungala00@gmail.com>
2025-11-28 18:41:19 +0530

    first commit

--- crud.js
+++ crud.js
@@ -1,23 +1,30 @@
-let players = [];
-
-function addplayers(name) {
-  players.push(name);
-}
-function showplayers() {
-  console.log(players);
-}
-function updateplayers(index, newName) {
-  players[index] = newName;
-}
-function deleteplayers(index) {
-  players.splice(index, 1);
-}
-addplayers('dhoni');
-addplayers('virat');
-addplayers('raina');
-showplayers();
-updateplayers(0, 'rohit');
-showplayers();
-
-deleteplayers(0);
-showplayers();
+const express = require('express');
+const app = express();
+const PORT = 3000;
+// GET /
+app.get('/', (req, res) => {
+  res.send('user done');
+});
+// GET /user
+// app.get('/user', (req, res) => {
+//   res.send('user');
+// });
+// POST /user
+app.post('/', (req, res) => {
+  let user = {};
+  res.send("User created: ");
+});
+// PUT /user
+app.put('/', (req, res) => {
+  let user = {};
+  res.send("User Updated");
+});
+// DELETE /user
+app.delete('/', (req, res) => {
+  let user = {};
+  res.send("User Deleted");
+});
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
